c896e889a3de7c780915193e177060201e36ee09
d.o.kononov <d.o.kononov@gmail.com>
2025-09-12 10:47:13 +0300

    add order view

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+@Observable
+final class Order {
+    static let types = ["Vanilla", "Chocolate", "Strawberry", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specilaRequestEnabled = false {
+        didSet {
+            if specilaRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+}
